fd97aebbc0e530e80198cfc5960163fe89689d8a
dtanglao-tech <dtanglao@byupathway.edu>
2026-05-20 20:20:01 +0800

    Week 2 temple project

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+const title = document.querySelector("h1");
+
+title.addEventListener("click", function () {
+    alert("Welcome to the Temple Album!");
+});
+
+document.getElementById("lastModified").textContent =
+"Last Modification: " + document.lastModified;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+
+});
